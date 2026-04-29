abdbc955acba7fb773d0d8d6b438621aa4554eb7
solehudin037 <solehudinfadillah@gmail.com>
2026-04-29 22:59:17 +0700

    upgrade6.5

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -42,7 +42,7 @@ const Sidebar = () => {
           </div>
         </div>
         <button className="theme-toggle" onClick={toggleTheme}>
-          🌙 Dark Mode
+          🌙
         </button>
       </div>
 
